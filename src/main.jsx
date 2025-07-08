fa5bb922086bd400ce1d989da4383f2879d0e950
FellowCactus <csaeztor@gmail.com>
2025-07-09 01:29:14 +0200

    viewport 2

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,6 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0"></meta>
     <App />
   </StrictMode>,
 )
